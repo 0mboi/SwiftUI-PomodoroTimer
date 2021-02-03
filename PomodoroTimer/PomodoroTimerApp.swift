fb22adf12faf5b039a23bd8ae542f340ec571cfc
Christopher Hicks <visrule@protonmail.com>
2021-02-03 15:14:32 -0800

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Christopher Hicks on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
